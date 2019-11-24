e3f8909dfc14c72d645761e01dbab414e676503d
ddayan0 <denday198@gmail.com>
2019-11-24 15:51:11 -0500

    added incomplete bankapp

---
+++ bankapp.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+
+}
+
+int menu(){
+
+
+
+}
+
+int new_acc(){
+
+
+
+}
+
+int view_list(){
+
+
+}
+int edit(){
+
+}
+
+
+int transact(){
+
+}
+int erase(){
+
+}
+
+int see(){
+
+
+
+}
+
+
+
